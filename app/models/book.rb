deac9319004101a7f77a35cf93a58b6f0696214e
Masaki-Ishida-1 <97374117+Masaki-Ishida-1@users.noreply.github.com>
2022-04-10 11:25:22 +0000

    [add]ActiveStorage画像格納

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :book_image
+
+  def get_book_image(width, height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      book_image.attach(io: File.open(file_path), filename: 'default-image.jpg',content_type:'image/jpeg')
+    end
+      book_image.variant(resize_to_fit:[width, height]).processed
+  end
 end
