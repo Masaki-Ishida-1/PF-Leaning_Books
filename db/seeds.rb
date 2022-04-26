0e7094bf8bdc2ea30177a1c76c0e0bbb681651ca
Masaki-Ishida-1 <97374117+Masaki-Ishida-1@users.noreply.github.com>
2022-04-26 12:57:43 +0000

    [fix]修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,4 +29,28 @@ admins.each do |admin|
       password: admin[:password]
     )
   end
+
+  User.create!(
+    [
+      {
+        family_name: '山田',
+        first_name: '太郎',
+        email: 'abcd@example.com',
+        password: 'password',
+        is_deleted: 0,
+        introduction: 'この一ヶ月でプログミングの学習をはじめました！',
+        image: File.open('./app/assets/images/example.jpg')
+      },
+      {
+        family_name: '田中',
+        first_name: '花子',
+        email: '1234@example.com',
+        password: 'password',
+        is_deleted: 0,
+        introduction: 'プログラムの勉強始めて3ヶ月です！',
+        image: File.open('./app/assets/images/example.jpg')
+      }
+    ]
+  )
+
 end
