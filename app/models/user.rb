9f2d0928437744ec191dea60250fa2600561bf7f
Masaki-Ishida-1 <97374117+Masaki-Ishida-1@users.noreply.github.com>
2022-04-26 10:23:15 +0000

    [fix]ゲスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,8 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "guestuser"
+      user.family_name = "guestuser"
+      user.first_name = "ゲスト"
     end
   end
 
