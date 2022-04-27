22e715f916f624b35a781dc730b61b81a137d115
Masaki-Ishida-1 <97374117+Masaki-Ishida-1@users.noreply.github.com>
2022-04-27 03:44:32 +0000

    [add]CSS

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -5,14 +5,18 @@ class Public::FavoritesController < ApplicationController
   end
 
   def create
-    @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.new(book_id: @book.id)
-    @favorite.save
+    if current_user.name != "guestuser"
+      @book = Book.find(params[:book_id])
+      @favorite = current_user.favorites.new(book_id: @book.id)
+      @favorite.save
+    end
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.find_by(book_id: @book.id)
-    @favorite.destroy
+    if current_user.name != "guestuser"
+      @book = Book.find(params[:book_id])
+      @favorite = current_user.favorites.find_by(book_id: @book.id)
+      @favorite.destroy
+    end
   end
 end
